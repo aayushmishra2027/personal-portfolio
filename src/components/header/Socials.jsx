5390da8e035ed0391e0328b84109ae75890d3c69
Aayush Mishra <aayushmishra1105@gmail.com>
2024-12-05 15:04:36 +0530

    Update Socials.jsx

--- src/components/header/Socials.jsx
+++ src/components/header/Socials.jsx
@@ -4,8 +4,8 @@ import {BsLinkedin, BsGithub} from 'react-icons/bs';
 const Socials = () => {
   return (
     <div className='header__socials'>
-       <a href="https://www.linkedin.com/in/lucasyannul/" target='_blank'><BsLinkedin/></a>
-       <a href="https://github.com/lyannul" target='_blank'><BsGithub/></a>
+       <a href="https://www.linkedin.com/in/aayush-anil-mishra-508abb280/" target='_blank'><BsLinkedin/></a>
+       <a href="https://github.com/aayushmishra2027" target='_blank'><BsGithub/></a>
     </div>
   );
 }
