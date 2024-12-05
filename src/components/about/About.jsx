080cb26812305d572984468246e05e229a09ef57
Aayush Mishra <aayushmishra1105@gmail.com>
2024-12-05 14:45:00 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,7 @@ import {BiMedal, BiUser, BiFolder} from 'react-icons/bi';
 const About = () => {
   return (
     <section id='about'>
-        <h5>Get to know</h5>
+        <h5>Learn More</h5>
         <h2>About Me</h2>
 
         <div className="container about__container">
@@ -20,28 +20,30 @@ const About = () => {
                     <article className='about__card'>
                         <BiMedal className='about__icon'/>
                         <h5>Experience</h5>
-                        <small>2 Summer SWE Internships</small>
+                        <small>Internships Done - 2</small>
                     </article>
                     <article className='about__card'>
                         <BiUser className='about__icon'/>
                         <h5>Education</h5>
-                        <small>Villanova University <br/> CpE and CS</small>
+                        <small>MIT Manipal <br/> Mechatronics Engineer</small>
                     </article>
                     <article className='about__card'>
                         <BiFolder className='about__icon'/>
-                        <h5>Hobbies</h5>
-                        <small>Traveling, Lacrosse, Basketball & Coding</small>
+                        <h5>Passionate About</h5>
+                        <small>Astronomy, Coding, Solo-Travelling</small>
                     </article>
                 </div>
                 <p>
-                    I am a senior Computer Engineering major and Computer Science 
-                    minor at Villanova! I am currently pursuing a career as a Software
-                    Engineer. I have a strong passion for both front-end
-                    and back-end development. I love to learn and I am always looking to learn
-                    and experiment with new technologies!
+                    I'm an Engineering Student by Profession and 
+                    currently pursuing my Bachelor's in Mechatronics
+                    (Robotics & Automation) at MIT Manipal (2023-27). 
+                    
+                    Besides Engineering, I'm also passionate about 
+                    Web-Development and Astronomy.I always look for 
+                    learning a concept through practical/hands-on application.
                 </p>
 
-                <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                <a href="#contact" className='btn btn-primary'>Let's Get in Touch!</a>
             </div>
         </div>
     </section>
